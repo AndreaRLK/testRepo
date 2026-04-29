b7fdccc33d12d198360cabb94a08723f5bd6f617
Andrea <241164093+AndreaRLK@users.noreply.github.com>
2026-04-29 01:25:29 -0400

    updated .js file and html file to link to the .js file correctly

--- JavaScript/scope_lab.js
+++ JavaScript/scope_lab.js
@@ -8,27 +8,30 @@ console.log(globalVar); // Output: "I'm a global variable"
 console.log(globalLet); // Output: "I'm also global, but scoped with let"
 console.log(globalConst); // Output: "I'm a global constant"
 
-
 {
-// Block scope
-let blockVar = "I'm block-scoped with let";
-let blockLet = "Block scoped let";
-const blockConst = "Block scoped constant";
+  // Block scope
+  let blockVar = "I'm block-scoped with let";
+  let blockLet = "Block scoped let";
+  const blockConst = "Block scoped constant";
 
-// Valid inside the block
-console.log(blockVar);
-console.log(blockLet);
-console.log(blockConst);
+  // Valid inside the block
+  console.log(blockVar);
+  console.log(blockLet);
+  console.log(blockConst);
 }
 
-function show(){
-    var functionVar = "I'm a block-scoped var";
-    let functionLet = "I'm a block-scoped let";
-    const functionConst = "I'm a block-scoped const";
-    }
-    show();
-    
-    // console.log(functionVar); Throws ReferenceError
-    // console.log(functionLet); Throws ReferenceError
-    // console.log(functionConst); Throws ReferenceError
-    
+function show() {
+  var functionVar = "I'm a function-scoped var";
+  let functionLet = "I'm scoped to the function body with let";
+  const functionConst = "I'm scoped to the function body with const";
+  
+  console.log(functionVar); // Throws ReferenceError
+  console.log(functionLet); // Throws ReferenceError
+  console.log(functionConst);
+}
+show();
+
+// Invalid outside the function scope
+console.log(functionVar); // Throws ReferenceError
+console.log(functionLet); // Throws ReferenceError
+console.log(functionConst); // Throws ReferenceError
